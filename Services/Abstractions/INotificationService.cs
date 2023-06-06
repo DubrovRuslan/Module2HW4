2411de714b886733715b16ee9c7394dd57210b03
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-06 11:21:05 +0300

    Services interfaces

---
+++ Services/Abstractions/INotificationService.cs
@@ -0,0 +1,10 @@
+﻿using Module2HW4.Models;
+
+namespace Module2HW4.Services.Abstractions
+{
+    public interface INotificationService
+    {
+        public void FindNotify(Animal[] animals, string messageToNotify);
+        public void CountNotify(int count, string message);
+    }
+}
