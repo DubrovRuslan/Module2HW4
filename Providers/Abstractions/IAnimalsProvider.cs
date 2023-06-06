fcd0aef96b420df961a1b8b726af8c218ba7538f
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-06 11:19:38 +0300

    Providers interfaces

---
+++ Providers/Abstractions/IAnimalsProvider.cs
@@ -0,0 +1,12 @@
+﻿using Module2HW4.Models;
+using Module2HW4.Models.Enums;
+
+namespace Module2HW4.Providers.Abstractions
+{
+    public interface IAnimalsProvider
+    {
+        public Animal[] GetAnimals();
+        public Animal[] GetAnimalByType(AnimalType type);
+        public void AddNewAnimal(Animal animal);
+    }
+}
