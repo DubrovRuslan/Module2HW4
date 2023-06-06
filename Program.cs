ba12c50a7ae42f9cf454fde7f8f66b1ff2a65c54
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-06 11:34:03 +0300

    top level

--- Program.cs
+++ Program.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Module2HW4.Providers;
 using Module2HW4.Services;
 using Module2HW4.Providers.Abstractions;
 using Module2HW4.Services.Abstractions;
 
-namespace Module2HW4
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var serviceProvider = new ServiceCollection()
+var serviceProvider = new ServiceCollection()
                 .AddTransient<IAnimalsProvider, AminalsProvider>()
                 .AddSingleton<IAnimalService, AnimalService>()
                 .AddSingleton<IConsoleNotificator, ConsoleNotificator>()
                 .AddSingleton<INotificationService, NotificationService>()
-                .AddTransient<Starter>()
+                .AddTransient<Module2HW4.Starter>()
                 .BuildServiceProvider();
-            var start = serviceProvider.GetService<Starter>();
-            start.Run();
-        }
-    }
-}
+var start = serviceProvider.GetService<Module2HW4.Starter>();
+start.Run();
