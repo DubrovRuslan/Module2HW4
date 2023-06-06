82c80bb36b7e7b2414e431b55bc4b8c126dda0b2
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-06 11:17:45 +0300

    Helpers done

---
+++ Helpers/AnimalExtension.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Module2HW4.Models;
+using Module2HW4.Models.Enums;
+
+namespace Module2HW4.Helpers
+{
+    public static class AnimalExtension
+    {
+        public static Animal GetAnimalByType(this Animal animal, AnimalType type)
+        {
+            Animal result = null;
+            if (animal.CurrentAnimalType == type)
+            {
+                result = animal;
+            }
+
+            return result;
+        }
+
+        public static Animal FindByName(this Animal animal, string name)
+        {
+            if (animal == null)
+            {
+                return null;
+            }
+
+            if (animal.Name.Contains(name))
+            {
+                return animal;
+            }
+
+            return null;
+        }
+
+        public static Animal FindByWeight(this Animal animal, double minWeight, double maxWeight)
+        {
+            if (animal == null)
+            {
+                return null;
+            }
+
+            if (animal.Weight >= minWeight && animal.Weight <= maxWeight)
+            {
+                return animal;
+            }
+
+            return null;
+        }
+
+        public static Animal FindByHeadPenetrationDepth(this Animal animal, double headPenetrationDepth)
+        {
+            var runningFarBird = animal as RunningFarBird;
+            if (runningFarBird == null)
+            {
+                return null;
+            }
+
+            if (runningFarBird.DepthOfPenetrationOfHeadIntoSand >= headPenetrationDepth)
+            {
+                return runningFarBird;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Helpers/ArrayHelper.cs
@@ -0,0 +1,58 @@
+﻿using Module2HW4.Models;
+
+namespace Module2HW4.Helpers
+{
+    public static class ArrayHelper
+    {
+        public static bool AddElementToArray(ref Animal[] array, Animal newElement)
+        {
+            if (newElement == null)
+            {
+                return false;
+            }
+
+            var currentSize = (array == null) ? 0 : array.Length;
+            var temp = new Animal[currentSize + 1];
+            for (var i = 0; i < currentSize; i++)
+            {
+                temp[i] = array[i];
+            }
+
+            temp[currentSize] = newElement;
+            array = temp;
+            return true;
+        }
+
+        public static bool DeleteElemnt(ref object[] array, int indexDeleteElemet)
+        {
+            if (array == null)
+            {
+                return false;
+            }
+
+            if (indexDeleteElemet < 0 || indexDeleteElemet > array.Length - 1)
+            {
+                return false;
+            }
+
+            if (array.Length == 1 && indexDeleteElemet == 0)
+            {
+                array = null;
+                return true;
+            }
+
+            var temp = new object[array.Length - 1];
+            for (int i = 0, j = 0; i < array.Length; i++)
+            {
+                if (i != indexDeleteElemet)
+                {
+                    temp[j] = array[i];
+                    j++;
+                }
+            }
+
+            array = temp;
+            return true;
+        }
+    }
+}
