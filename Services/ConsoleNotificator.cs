53a7d1d0cf5b591da2c9831f72160c45b7d3d192
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-06 11:21:30 +0300

    Services done

---
+++ Services/ConsoleNotificator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Module2HW4.Services.Abstractions;
+
+public class ConsoleNotificator : IConsoleNotificator
+{
+    public void WriteToConsole(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
